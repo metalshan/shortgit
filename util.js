2cd12265751779b55c3646c7260623ff14833ed8
metalshan <go2mailshan@gmail.com>
2017-07-14 21:39:35 +0530

    "bug fix utils"

--- util.js
+++ util.js
@@ -2,7 +2,7 @@ const shell = require("./shell");
 
 module.exports = {
   getCurrentBranch: () => {
-    const branchName = shell.execSync("git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ \1/'").toString();
+    let branchName = shell.execSync("git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ \1/'").toString();
     branchName = branchName.replace("*", "").trim();
     return branchName;
   }
