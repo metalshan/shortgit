262ac12cc657e3f211005bd695c8d10dc1860148
metalshan <go2mailshan@gmail.com>
2017-07-14 18:02:18 +0530

    "fixing execSync"

--- bin/gpl.js
+++ bin/gpl.js
@@ -4,7 +4,7 @@ const args = require("yargs").argv._;
 let addedParams = "";
 switch(args.length){
     case 0:
-        let branchName = shell.exec("branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ \1/'".split(" "));
+        let branchName = shell.execSync("git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ \1/'").toString();
         branchName = branchName.replace("*", "").trim();
         addedParams = ["origin", branchName];
         break;
--- shell.js
+++ shell.js
@@ -1,9 +1,10 @@
 const spawn = require("child_process").spawn;
 const exec = require("child_process").exec;
+const execSync = require("child_process").execSync;
 
 module.exports = {
     exec(commands){
-        console.log(commands)
         spawn("git", commands, {stdio:'inherit'})
-    }
+    },
+    execSync
 }
